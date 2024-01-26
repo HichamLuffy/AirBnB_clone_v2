8e0277efa278abc17f98a7f3ad4deba396c275bd
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-26 17:40:36 +0100

    fix pycodestyle

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,10 +38,10 @@ def number(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-
 def number_template(n):
     """ def number template """
     return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
