494e3581e00d472b4e95c81905a01d18e7e7f774
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-26 17:36:44 +0100

    number odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module numbr roote """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -34,6 +35,23 @@ def python(text):
 def number(n):
     """ def number """
     return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+
+def number_template(n):
+    """ def number template """
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """ def number template """
+    if n % 2 == 0:
+        answer = "even"
+    else:
+        answer = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, answer=answer)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
