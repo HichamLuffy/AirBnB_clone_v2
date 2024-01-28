74d8aff90ced597b57da548ab3c29f2caa680d70
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-28 17:50:40 +0100

    fix html and py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,6 +9,67 @@ from models import *
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def hellowww """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ def c """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """ def python """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ def number """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ def number template """
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """ def number template """
+    if n % 2 == 0:
+        answer = "even"
+    else:
+        answer = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, answer=answer)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ def doc """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ def doc """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.route("/states", strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
 def states(id=None):
